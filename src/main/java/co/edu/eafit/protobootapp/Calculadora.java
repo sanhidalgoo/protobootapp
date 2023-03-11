f1559bbf2ca45b0f5ba335d963a7a9b5db97c062
Santiago Hidalgo <santiago.hidalgo.ext@holcim.com>
2023-03-11 08:48:39 -0500

    🔥 Removed spaces

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -6,7 +6,7 @@ public class Calculadora {
     public Double cuadrado(Double numero) {
         return numero * numero;
     }
-    
+
     public Double cubo(Double numero) {
         return numero * numero * numero;
     }
